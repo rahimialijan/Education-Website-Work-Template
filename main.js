c540c9d8f40e7dc67991a143b18d057579b1f417
rahimialijan <alijanrahimi24@yahoo.com>
2023-02-21 23:33:22 +0500

    Mobile Menu and Navigation Bar style

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const nav =document.querySelector("nav");
-
+const menuIcon  =document.querySelector(".menu-icon");
+const closeIcon = document.querySelector (".mobile-menu-container .close-icon");
+const mobileMenuContainer =document.querySelector(".mobile-menu-container");
+const mobileNav =document.querySelector("nav.mobile-nav");
 window.addEventListener("scroll",() =>{
     if (window.pageYOffset>60){
         nav.classList.add("scrolled");
+        mobileNav.classList.add("scrolled");
     }else{
         nav.classList.remove("scrolled")
+        mobileNav.classList.remove("scrolled")
     }
 });
+menuIcon.addEventListener("click", () =>{
+mobileMenuContainer.classList.add("active")
+});
+closeIcon.addEventListener("click", ()=>{
+mobileMenuContainer.classList.remove("active")
+});
+
