631fa1c6e648b030908b4993c7daff51c11a3b6c
rahimialijan <alijanrahimi24@yahoo.com>
2023-02-22 19:40:21 +0500

    Styling for Small phone

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const menuIcon  =document.querySelector(".menu-icon");
 const closeIcon = document.querySelector (".mobile-menu-container .close-icon");
 const mobileMenuContainer =document.querySelector(".mobile-menu-container");
 const mobileNav =document.querySelector("nav.mobile-nav");
+
 window.addEventListener("scroll",() =>{
     if (window.pageYOffset>60){
         nav.classList.add("scrolled");
@@ -12,6 +13,7 @@ window.addEventListener("scroll",() =>{
         mobileNav.classList.remove("scrolled")
     }
 });
+
 menuIcon.addEventListener("click", () =>{
 mobileMenuContainer.classList.add("active")
 });
